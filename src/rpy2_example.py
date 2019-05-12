3a2da60f6cd83efdb31e8e90f91e5cd0fa60a1fb
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-05-12 17:16:02 -0400

    Update rpy2_example.py

--- src/rpy2_example.py
+++ src/rpy2_example.py
@@ -9,7 +9,16 @@ import rpy2.rinterface
 from rpy2.robjects.packages import importr
 #from rpy2.robjects.packages import importr
 
-base = importr('base')
+base = importr('base', lib_loc = "C:/Users/steve/OneDrive/Documents/R/win-library/3.5")
 print(base._libPaths()) #[1] "C:/Users/steve/Anaconda3/Lib/R/library"
 
+
+# In R: install.packages("lattice", lib="C:/Users/steve/Anaconda3/Lib/R/library", dependencies=TRUE)
 quanteda = importr("quanteda", lib_loc = "C:/Users/steve/OneDrive/Documents/R/win-library/3.5")
+#quanteda.chooseCRANmirror(ind=1)
+
+x = quanteda.textstat_readability('The house is cool', measure = 'ARI')[1]
+x.r_repr()
+
+type(quanteda.textstat_readability('The house is cool', measure = 'ARI')[1].r_repr())
+ari_score = float(quanteda.textstat_readability('The house is cool', measure = 'ARI')[1].r_repr())
