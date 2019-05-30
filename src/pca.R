fe472144224895e2516c73edd00297ec11fd02c3
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2019-05-29 21:19:51 -0400

    Update pca.R

--- src/pca.R
+++ src/pca.R
@@ -1,5 +1,54 @@
-rm(list=ls())
+### This script applies PCA to the state court readability measures.
 
+rm(list=ls())
 setwd("C:/Users/steve/Dropbox/PSU2018-2019/RA/CAP")
 
+library(factoextra)
+
+# Read in data
 state.courts <- read.csv('state_court_wide_final_bottom_half5-29.csv')
+
+# Omit na values
+#read.metrics <- na.omit(state.courts[,15:32])
+
+# Deal w/ infinte values
+x <- read.metrics[rowSums(is.infinite(as.matrix(read.metrics))) == 0, ]
+
+# Calculate singular value decomposition
+read.pca <- prcomp(x, scale = TRUE)
+
+# Visualize eigenvalues (scree plot)
+fviz_eig(read.pca)
+ggsave('scree_plot_18.png')
+
+# Group by state, graph
+#fviz_pca_var(read.pca,
+#             col.var = "contrib", # Color by contributions to the PC
+#             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+#             repel = TRUE     # Avoid text overlapping
+#)
+
+# Graph biplot correlation of variables
+fviz_pca_var(read.pca,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+ggsave('var_biplot.png')
+
+
+### Access PCA results
+# Eigenvalues
+eig.val <- get_eigenvalue(read.pca)
+eig.val
+
+# Results for Variables
+res.var <- get_pca_var(read.pca)
+res.var$coord          # Coordinates
+res.var$contrib        # Contributions to the PCs
+res.var$cos2           # Quality of representation 
+# Results for individuals
+res.ind <- get_pca_ind(read.pca)
+res.ind$coord          # Coordinates
+res.ind$contrib        # Contributions to the PCs
+res.ind$cos2           # Quality of representation 
