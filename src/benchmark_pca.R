6ece1a920454c3ad2298eddcb56fab0bc178ebff
Steven Morgan <33168464+stevenjmorgan@users.noreply.github.com>
2020-07-06 19:25:56 -0700

    Update benchmark_pca.R

--- src/benchmark_pca.R
+++ src/benchmark_pca.R
@@ -102,7 +102,7 @@ ggplot(coord, aes(x=dim1)) + geom_histogram(color="darkblue", fill="lightblue") 
 gc()
 
 # Add first dim. values as variable
-read.metrics$first.dim <- coord$dim1
+corpus$first.dim <- coord$dim1
 
 
 # top.example <- all.courts[max(all.courts$first.dim),]
@@ -126,12 +126,13 @@ read.metrics$first.dim <- coord$dim1
 # top.example$cite
 
 
-# Group by year-state median, sort by state
-all.courts$state <- as.character(all.courts$state)
-year_state1d <- aggregate(all.courts[,c('first.dim')], 
-                          list(all.courts$state, all.courts$year), median)
-colnames(year_state1d)[1] <- 'state'
-colnames(year_state1d)[2] <- 'year'
+# Group by year/decade median
+year.1d <- aggregate(corpus[,c('first.dim')], 
+                          list(corpus$year), median)
+colnames(year.1d)[1] <- 'year'
+dec.1d <- aggregate(corpus[,c('first.dim')], 
+                    list(corpus$decade), median)
+colnames(dec.1d)[1] <- 'decade'
 
 
 # Plot first dimension measure by year and state
@@ -139,7 +140,7 @@ colnames(year_state1d)[2] <- 'year'
 plot(year_state1d$year, year_state1d$x) # This is stupid, it's PCA, shouldn't plot aggregates
 plot(all.courts$year, all.courts$x) # Lol this is dumber
 
-ggplot(data=year_state1d, aes(x=year,y=x)) + geom_point()
+ggplot(data=year.1d, aes(x=year,y=x)) + geom_point()
 
 summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
                       conf.interval=.95, .drop=TRUE) {
@@ -176,45 +177,29 @@ summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
   
   return(datac)
 }
-tgc <- summarySE(all.courts[which(all.courts$state == 'Maryland'),], 
+tgc <- summarySE(corpus, 
                  measurevar="first.dim" , groupvars=c("year"))
 
 # Standard error of the mean
-ggplot(tgc[which(tgc$year > 1775),], aes(x=year, y=first.dim)) + 
+ggplot(tgc, aes(x=year, y=first.dim)) + 
   geom_errorbar(aes(ymin=first.dim-se, ymax=first.dim+se), width=.1) +
   #geom_line() +
   geom_point() + xlab('Year') + ylab('Readability') +
-  theme_bw() + 
-  theme(text = element_text(size=25))
-ggsave('MD_first_dim.png')
+  theme_bw() + ggtitle('PCA Readability Scores by Year: US Benchmark Corpus')
+ggsave('year_benchmark_first_dim.png')
 #ggplot(data=all.courts[which(all.courts$state == 'Massachusetts'),], aes(x=year,y=first.dim)) + geom_point()
 
-tgc <- summarySE(all.courts, 
-                 measurevar="first.dim" , groupvars=c("year"))
-ggplot(tgc[which(tgc$year > 1775),], aes(x=year, y=first.dim)) + 
-  geom_errorbar(aes(ymin=first.dim-se, ymax=first.dim+se), width=.1) +
-  #geom_line() +
-  geom_point() + xlab('Year') + ylab('Readability') +
-  theme_bw() + 
-  theme(text = element_text(size=25))
-ggsave('all_years_first_dim.png')
 
-
+# Decade
+tgc <- summarySE(corpus, 
+                 measurevar="first.dim" , groupvars=c("decade"))
 
 # Standard error of the mean
-
-###
-load('combined_read_metrics.RData')
-
-library(plyr)
-
-all.courts$state <- as.character(all.courts$state)
-freq <- plyr::count(all.courts, vars=c("year","state"))
-year_state1d <- merge(year_state1d, freq, by = c('state','year'), all.x = TRUE)
-year_state1d <- year_state1d[order(year_state1d$state, year_state1d$year),]
-View(year_state1d)
-
-save(year_state1d, file = 'year_state_measures.RData')
-
-
+ggplot(tgc, aes(x=decade, y=first.dim)) + 
+  geom_errorbar(aes(ymin=first.dim-se, ymax=first.dim+se), width=.1) +
+  #geom_line() +
+  geom_point() + xlab('Decade') + ylab('Readability') +
+  theme_bw() + ggtitle('PCA Readability Scores by Decade: US Benchmark Corpus')
+ggsave('dec_benchmark_first_dim.png')
+#ggplot(data=all.courts[which(all.courts$state == 'Massachusetts'),], aes(x=year,y=first.dim)) + geom_point()
 
